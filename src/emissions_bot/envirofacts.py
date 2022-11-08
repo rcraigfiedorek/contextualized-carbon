73d8e0b5b9b0b682a908fa2e3cae71755b97f8ff
Craig Fiedorek <rcraigfiedorek@gmail.com>
2022-11-08 13:46:27 -0500

    Finished envirofacts db query class

--- src/emissions_bot/envirofacts.py
+++ src/emissions_bot/envirofacts.py
@@ -1,63 +1,104 @@
 from __future__ import annotations
 
 import dataclasses
+import io
 from typing import List, Literal, Optional
+
+import requests
+import pandas as pd
 
 
 ENVIROFACTS_URL = 'https://data.epa.gov/efservice'
 
 
 class Query:
-    tables: List[_Table]
 
-    def __init__(self):
+    tables: List[TableQueryData]
+
+    def __init__(self, table_name: str | None = None):
         self.tables = list()
-    
+        if table_name:
+            self.tables.append(TableQueryData(table_name))
+
+    @property
+    def url(self):
+        return ENVIROFACTS_URL + ''.join(table.url_path for table in self.tables) + '/CSV'
+
     def table(self, table_name: str):
-        self.tables.append(_Table(table_name))
+        self.tables.append(TableQueryData(table_name))
         return self
-    
+
     def column_equals(self, column_name: str, column_value: str):
-        self.tables[-1].column_name = column_name
-        self.tables[-1].column_value = column_value
-        self.tables[-1].operator = '='
+        self.tables[-1].filters.append(ColumnFilterData(
+            column_name=column_name,
+            operator='=',
+            column_value=column_value
+        ))
         return self
-    
+
     def column_nequals(self, column_name: str, column_value: str):
-        self.tables[-1].column_name = column_name
-        self.tables[-1].column_value = column_value
-        self.tables[-1].operator = '!='
+        self.tables[-1].filters.append(ColumnFilterData(
+            column_name=column_name,
+            operator='!=',
+            column_value=column_value
+        ))
         return self
-    
+
     def column_less(self, column_name: str, column_value: str):
-        self.tables[-1].column_name = column_name
-        self.tables[-1].column_value = column_value
-        self.tables[-1].operator = '<'
+        self.tables[-1].filters.append(ColumnFilterData(
+            column_name=column_name,
+            operator='<',
+            column_value=column_value
+        ))
         return self
-    
+
     def column_greater(self, column_name: str, column_value: str):
-        self.tables[-1].column_name = column_name
-        self.tables[-1].column_value = column_value
-        self.tables[-1].operator = '>'
+        self.tables[-1].filters.append(ColumnFilterData(
+            column_name=column_name,
+            operator='>',
+            column_value=column_value
+        ))
         return self
-    
+
     def column_begins(self, column_name: str, column_value: str):
-        self.tables[-1].column_name = column_name
-        self.tables[-1].column_value = column_value
-        self.tables[-1].operator = 'BEGINNING'
+        self.tables[-1].filters.append(ColumnFilterData(
+            column_name=column_name,
+            operator='BEGINNING',
+            column_value=column_value
+        ))
         return self
-    
+
     def column_contains(self, column_name: str, column_value: str):
-        self.tables[-1].column_name = column_name
-        self.tables[-1].column_value = column_value
-        self.tables[-1].operator = 'CONTAINING'
+        self.tables[-1].filters.append(ColumnFilterData(
+            column_name=column_name,
+            operator='CONTAINING',
+            column_value=column_value
+        ))
         return self
-    
+
+    def get(self) -> pd.DataFrame:
+        with io.StringIO() as buf:
+            buf.write(requests.get(self.url).text)
+            buf.seek(0)
+            return pd.read_csv(buf)
+
 
 @dataclasses.dataclass
-class _Table:
+class TableQueryData:
     table_name: str
-    column_name: str | None = None
-    operator: Literal['=', '!=', '<', '>', 'BEGINNING', 'CONTAINING'] | None = None
-    column_value: str | None = None
+    filters: List[ColumnFilterData] = dataclasses.field(default_factory=list)
 
+    @property
+    def url_path(self):
+        return f'/{self.table_name}' + ''.join(filter.url_path for filter in self.filters)
+
+
+@dataclasses.dataclass
+class ColumnFilterData:
+    column_name: str
+    operator: Literal['=', '!=', '<', '>', 'BEGINNING', 'CONTAINING']
+    column_value: str
+
+    @property
+    def url_path(self):
+        return f'/{self.column_name}/{self.operator}/{self.column_value}'
