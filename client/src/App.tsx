4df93a66ac7706ed7d3a6878bb88ac3b914b118b
Craig Fiedorek <rcraigfiedorek@gmail.com>
2022-12-07 01:26:00 -0500

    Beginning transition to sidebar form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import _ from "lodash";
-import { useEffect, useState } from "react";
-import { CompanyOutput } from "./api";
 import "./App.css";
 import { CompanyEmissionInfo } from "./components/CompanyEmissionInfo";
 import { NavBar } from "./components/NavBar";
-import { api } from "./config";
 
 function App() {
-  const [initCompany, setInitCompany] = useState<CompanyOutput>();
-
-  useEffect(() => {
-    api
-      .apiCompaniesGet(1, 40, undefined, 2021, "fully_owned_emissions", 2021)
-      .then(({ data: { companies } }) => setInitCompany(_.sample(companies)));
-  }, []);
-
   return (
     <div className="App">
       <NavBar />
@@ -23,6 +11,8 @@ function App() {
       <div className="App-body">
         <CompanyEmissionInfo />
       </div>
+
+      <div className="search-menu-toggle">Search</div>
     </div>
   );
 }
