933777621cb4b9471fa743d2adb7b8724a073aec
Craig Fiedorek <rcraigfiedorek@gmail.com>
2022-12-08 10:08:02 -0500

    add secret pull to instance startup script. Add DB name to prod docker compose. Use docker's github action step for build/push, in order to set up cache later.

--- google/compute/instance-startup.sh
+++ google/compute/instance-startup.sh
@@ -1,3 +1,4 @@
-sudo docker compose -f /home/rcraigfiedorek/EmissionsBot/docker-compose.prod.yml down --remove-orphans
-sudo docker compose -f /home/rcraigfiedorek/EmissionsBot/docker-compose.prod.yml pull
-sudo docker compose -f /home/rcraigfiedorek/EmissionsBot/docker-compose.prod.yml up --detach
+export EMISSIONS_DB_PASSWORD=$(gcloud secrets versions access 1 --secret="EMISSIONS_DB_PASSWORD")
+sudo -E docker compose -f /home/rcraigfiedorek/EmissionsBot/docker-compose.prod.yml down --remove-orphans
+sudo -E docker compose -f /home/rcraigfiedorek/EmissionsBot/docker-compose.prod.yml pull
+sudo -E docker compose -f /home/rcraigfiedorek/EmissionsBot/docker-compose.prod.yml up --detach
