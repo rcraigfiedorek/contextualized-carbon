3b1600f7e4af9fd49a42ad5661a5a7c2433c64f4
Craig Fiedorek <rcraigfiedorek@gmail.com>
2022-12-08 13:56:41 -0500

    Starting HTTPS configuration

--- google/compute/instance-startup.sh
+++ google/compute/instance-startup.sh
@@ -1,4 +1,4 @@
 export EMISSIONS_DB_PASSWORD=$(gcloud secrets versions access 1 --secret="EMISSIONS_DB_PASSWORD")
 sudo -E docker compose -f /home/rcraigfiedorek/EmissionsBot/docker-compose.prod.yml down --remove-orphans
 sudo -E docker compose -f /home/rcraigfiedorek/EmissionsBot/docker-compose.prod.yml pull
-sudo -E docker compose -f /home/rcraigfiedorek/EmissionsBot/docker-compose.prod.yml up --detach
+sudo -E docker compose -f /home/rcraigfiedorek/EmissionsBot/docker-compose.prod.yml up -d db server client
